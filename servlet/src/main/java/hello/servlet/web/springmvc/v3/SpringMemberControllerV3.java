35e89c8fc43ebbf7c5c941e7c1fea842e88be269
이준희 <lee@ijunhuiui-MacBookPro.local>
2022-07-05 19:00:23 +0900

    springV3

--- servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,22 +17,22 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping("/new-form")
     public String newForm(){
         return "new-form";
     }
 
-    @RequestMapping
+    @GetMapping
     public String members(Model model) {
 
         List<Member> members = memberRepository.findAll();
 
         model.addAttribute("members", members);
-        
+
         return "members";
     }
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model){
 
         Member member = new Member(username, age);
