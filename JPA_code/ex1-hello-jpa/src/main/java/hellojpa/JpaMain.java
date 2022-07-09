958ac7720793e859e93148aa843407f5ccb103be
JunHee <ljh19980823@gmail.com>
2022-07-09 15:43:06 +0900

    JPA 실습

--- JPA_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -9,8 +10,33 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-        // code
-        em.close();
+
+        EntityTransaction ts = em.getTransaction();
+        ts.begin();
+
+        try{
+            // 저장
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("KyungTae");
+            em.persist(member);
+
+            // 조회
+            Member findMember = em.find(Member.class, 1L);
+            Member removeMember = em.find(Member.class, 2L);
+
+            // 삭제
+            em.remove(removeMember);
+            
+            // 변경
+            findMember.setName("Jun");
+            ts.commit();
+        } catch (Exception e){
+            ts.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
