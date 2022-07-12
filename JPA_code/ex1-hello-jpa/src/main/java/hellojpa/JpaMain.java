12d3961f958b98a292863c33cc44acdeb9d50e94
JunHee <ljh19980823@gmail.com>
2022-07-12 23:08:27 +0900

    JPA Study

--- JPA_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,6 @@ public class JpaMain {
         ts.begin();
 
         try{
-            // 비영속
-            Member member1 = new Member(10L, "JunHee");
-            Member member2 = new Member(11L, "KyungTae");
-            // 영속
-            em.persist(member1);
-            em.persist(member2);
-            // 준영속
-            //em.detach(member);
-            //DB에 저장
             ts.commit();
         } catch (Exception e){
             ts.rollback();
